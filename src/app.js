b0c8d7acb1d150e39ebd779b5249b067d4b79696
ylingene <ylingene@umich.edu>
2018-06-18 23:59:38 -0400

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,19 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 3 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 1000, createdAt: 1 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 2 }));
-// store.dispatch(setTextFilter('water'));
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'));
-// }, 3000);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 // Provider gives access to the redux store for all compomnents in it
 // if Overall component (eg. our router) given, then all components have access
 const jsx = (
