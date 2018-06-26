7150662d1cea4682964780ab1c82ceb810ff5d46
ylingene <ylingene@umich.edu>
2018-06-26 16:52:32 -0400

    add expensify styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
 export const ExpensesSummary = ({ expensesCount, expensesTotal }) => (
-    <div>
-        <p>{`Viewing ${expensesCount} expense${expensesCount === 1 ? '' : 's'} totalling ${numeral(expensesTotal / 100).format('$0,0.00')}`}</p>
+    <div className="page-header">
+        <div className="content-container">
+            <h1 className="page-header__title">Viewing <span>{expensesCount}</span> expense{expensesCount === 1 ? '' : 's'} totalling <span>{numeral(expensesTotal / 100).format('$0,0.00')}</span></h1>
+            <div className="page-header__actions">
+                <Link className="button" to="create">Add Expense</Link>
+            </div>
+        </div>
     </div>
 );
 
